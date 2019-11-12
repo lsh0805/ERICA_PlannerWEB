93251a1cb94291c668aa2ac650cf4cc7a97ce07c
lolmc <illionaire1004@gmail.com>
2019-11-13 05:40:47 +0900

    html의 쿠키 값 확인하여 로그인 여부에 따라 UI 변화

--- client/src/components/Topbar.js
+++ client/src/components/Topbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -12,8 +12,9 @@ import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
-import { Link } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import './css/Topbar.css'
+import axios from 'axios';
 
 const useStyles = makeStyles(theme => ({
   '@global': {
@@ -58,18 +59,66 @@ const useStyles = makeStyles(theme => ({
 
 
 const Topbar = () => {
-    const classes = useStyles();
+    let history = useHistory();
+    let location = useLocation();
+    const classes = useStyles(); 
+    const [isLoggedIn, setLogin] = React.useState(false);
     const [state, setState] = React.useState({
         top: false,
         left: false,
         bottom: false,
         right: false,
     });
+    useEffect(() => {
+        console.log(1111);
+        // 쿠키 값 가져옴
+        function getCookie(name) {
+            var value = "; " + document.cookie;
+            var parts = value.split("; " + name + "=");
+            if (parts.length == 2) return parts.pop().split(";").shift();
+        }
+
+        // 쿠키로 로그인 정보 가져옴
+        let loginData = getCookie('key');
+
+        // 로그인 정보가 없을 시 login = false
+        if(typeof loginData === "undefined"){ 
+            setLogin(false);
+            return;
+        }
+
+        // 로그인 데이터를 base64로 디코딩하고 JSON으로 parse
+        loginData = JSON.parse(atob(loginData));
+        console.log(loginData);
+        // 로그인 하지 않았다면 login = false
+        if(!loginData.isLoggedIn){ 
+            setLogin(false);
+            return;
+        }
+
+        // 로그인 했다면 login = true;
+        setLogin(true);
+    });
+    const handleLogout = () => {
+        let { from } = location.state || { from: { pathname: "/" } };
+        axios.get('/api/account/logout').then(() => {
+            let loginData = {
+                isLoggedIn: false,
+                username: ''
+            };
+
+            document.cookie = 'key=' + btoa(JSON.stringify(loginData));
+            setLogin(false);
+            history.push(from);
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
     const toggleDrawer = (side, open) => event => {
         if (event.type === 'keydown' && (event.key ===  'Tab' || event.key === 'Shift')) {
           return;
         }
-        setState({ ...state, [side]: open });
+        setState({ ...state, [side]: open }); //TODO: state 객체의 property로 side: open값을 추가하는 것인가? 알아보기 (ES6문법 공부)
     };
     
     const sideList = side => (
@@ -96,15 +145,41 @@ const Topbar = () => {
               </ListItem>
             ))}
           </List>
+    </div>
+    );
+    const loginUI = (
+        <div>
+            <Link to="/login" className="">
+                <Button color="primary" variant="outlined" className={classes.button}>
+                    로그인
+                </Button>
+            </Link>
+            <Link to="/register" className="">
+                <Button color="primary" variant="outlined" className={classes.button}>
+                    회원가입
+                </Button>
+            </Link>
         </div>
     );
+    const logoutUI = (
+        <div>
+            <Link to="/logout" className="">
+                <Button color="primary" variant="outlined" className={classes.button} onClick={handleLogout}>
+                    로그아웃
+                </Button>
+            </Link>
+        </div>
+    );
+    
     return (
       <React.Fragment>
         <AppBar color="default" elevation={0} className={classes.appBar}>
           <Toolbar id="top_bar" className={classes.topBar}>
             <div className="bar_left">
                 <Link to="/" className="logo">로고</Link>
-                <MenuIcon  onClick={toggleDrawer('left', true)}></MenuIcon>
+                <Button>
+                    <MenuIcon onClick={toggleDrawer('left', true)}></MenuIcon>
+                </Button>
             </div>
             <nav className="bar_nav">
                 <Link to="/" className="item">설명</Link>
@@ -113,12 +188,7 @@ const Topbar = () => {
                 <Link to="/" className="item">게시판</Link>
             </nav>
             <div className="bar_right">
-                <Button href="" color="primary" variant="outlined" className={classes.button}>
-                    <Link to="/login" className="">로그인</Link>
-                </Button>
-                <Button color="primary" variant="outlined" className={classes.button}>
-                    <Link to="/register" className="">회원가입</Link>
-                </Button>
+                { isLoggedIn ? logoutUI : loginUI }
             </div>
           </Toolbar>
         </AppBar>

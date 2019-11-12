bcbd118fba3baa1238118c1ebb8a0d3ca515277e
lolmc <illionaire1004@gmail.com>
2019-11-13 05:40:15 +0900

    axios로 서버와 통신 하여 회원가입 / 로그인 구현

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,172 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import axios from 'axios';
+import { useHistory, useLocation } from 'react-router-dom';
 
-const Login = () => {
-    return (
-        <div>
-            <h2>Login</h2>
+const AUTH_LOGIN_SUCCESS = "AUTH_LOGIN_SUCCESS";
+const AUTH_LOGIN_FAILURE = "AUTH_LOGIN_FAILURE";
+const AUTH_LOGIN = "AUTH_LOGIN";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Login() {
+    let history = useHistory();
+    let location = useLocation();
+    const classes = useStyles();
+    const [input_state, inputSetState] = useState({
+        email: "",
+        password: "",
+        keepLogin: false,
+    });
+    // TODO: 서버에서 입력 데이터 올바른 것인지 DB에서 검색한 결과를 토대로
+    // 인풋박스 아래에 오류메시지 출력하는 기능 추가.
+    function handleChange(e) {
+        inputSetState((prevStatus) => {
+            console.log(e);
+            if(e.target.name == "keepLogin")
+                prevStatus[e.target.name] = e.target.checked;
+            else
+                prevStatus[e.target.name] = e.target.value;
+            console.log(prevStatus);
+            return prevStatus;
+        });
+    }
+    // API REQUEST
+    const handleLogin = () => {
+        let { from } = location.state || { from: { pathname: "/" } };
+        let data = input_state;
+        axios.post('/api/account/login', data)
+        .then((response) => {
+            // create session data
+            let loginData = {
+                isLoggedIn: true,
+                email: data.email
+            };
+
+            document.cookie = 'key=' + btoa(JSON.stringify(loginData));
+
+            console.log("로그인 완료");
+            history.push(from);
+            return true;
+        }).catch((error) => {
+            console.log(error);
+            return false;
+        });
+    }
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          로그인
+        </Typography>
+        <div className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="이메일주소"
+            name="email"
+            autoComplete="email"
+            onChange={ handleChange }
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="비밀번호"
+            type="password"
+            id="password"
+            onChange={ handleChange }
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox name="keepLogin" color="primary" onChange={ handleChange } />}
+            label="로그인 유지"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={ handleLogin }
+            className={classes.submit}
+          >
+            로그인하기
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                비밀번호를 잊으셨나요?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/register" variant="body2">
+                회원가입
+              </Link>
+            </Grid>
+          </Grid>
         </div>
-    );
-};
-
-export default Login;
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component, useState, useEffect}from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -12,6 +12,26 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import axios from 'axios';
+import { useHistory, useLocation } from 'react-router-dom';
+// ERROR TPYES
+
+const ERR_ALREADY_USED_EMAIL = 1;
+const ERR_TOO_SHORT_EMAIL = 2;
+const ERR_INVALID_EMAIL = 3;
+const ERR_ALREADY_USED_NICKNAME = 4;
+const ERR_TOO_SHORT_NICKNAME = 5;
+const ERR_INVALID_NICKNAME = 6;
+const ERR_TOO_SHORT_PASSWORD = 7;
+const ERR_INVALID_PASSWORD = 8;
+const ERR_NOT_EQUAL_PASSWORD = 9;
+const DATA_EMAIL = 1;
+const DATA_NICKNAME = 2;
+const DATA_PASSWORD = 3;
+const DATA_PASSWORD_CHECK = 4;
+const AUTH_REGISTER_SUCCESS = "AUTH_REGISTER_SUCCESS";
+const AUTH_REGISTER_FAILURE = "AUTH_REGISTER_FAILURE";
+const AUTH_REGISTER = "AUTH_REGISTER";
 
 function Copyright() {
   return (
@@ -51,42 +71,79 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const Register = () => {
-  const classes = useStyles();
+export default function Register() {
+    let history = useHistory();
+    let location = useLocation();
+    const [register_state, regSetState] = useState({
+        loginFailed: false,
+        err: ""
+    });
+    const [input_state, inputSetState] = useState({
+        email: "",
+        password: "",
+        password_check: "",
+        nickname: "",
+        alarm: false,
+    });
+    // TODO: 서버에서 입력 데이터 올바른 것인지 DB에서 검색한 결과를 토대로
+    // 인풋박스 아래에 오류메시지 출력하는 기능 추가.
+    function handleChange(e) {
+        inputSetState((prevStatus) => {
+            console.log(e.target);
+            if(e.target.name == "alarm")
+                prevStatus[e.target.name] = e.target.checked;
+            else
+                prevStatus[e.target.name] = e.target.value;
+            console.log(prevStatus);
+            return prevStatus;
+        });
+    }
+    // API REQUEST
+    const handleRegister = () => {
+        let { from } = location.state || { from: { pathname: "/" } };
+        let data = input_state;
+        axios.post('/api/account/register', data)
+        .then((response) => {
+            // create session data
+            let loginData = {
+                isLoggedIn: true,
+                email: data.email
+            };
 
-  return (
-    <Container component="main" maxWidth="xs">
+            document.cookie = 'key=' + btoa(JSON.stringify(loginData));
+
+            console.log("회원가입 완료");
+            history.push('/');
+            return true;
+        }).catch((error) => {
+            console.log(error);
+            return false;
+        });
+    }
+    
+    const classes = useStyles();
+    return(
+    <Container component="main" xs={20} maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Sign up
+          회원가입
         </Typography>
-        <form className={classes.form} noValidate>
+        <div className={classes.form}>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="outlined"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
+            <Grid item xs={12}>
               <TextField
                 variant="outlined"
                 required
                 fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
+                id="nickname"
+                label="닉네임"
+                name="nickname"
+                onChange={ handleChange }
+                autoComplete="name"
               />
             </Grid>
             <Grid item xs={12}>
@@ -95,27 +152,42 @@ const Register = () => {
                 required
                 fullWidth
                 id="email"
-                label="Email Address"
+                label="이메일 주소"
                 name="email"
+                onChange={ handleChange }
                 autoComplete="email"
               />
             </Grid>
-            <Grid item xs={12}>
+            <Grid item xs={12} sm={6}>
               <TextField
                 variant="outlined"
                 required
                 fullWidth
                 name="password"
-                label="Password"
+                label="비밀번호"
                 type="password"
                 id="password"
+                onChange={ handleChange }
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password_check"
+                label="비밀번호 (확인)"
+                type="password"
+                id="password_check"
+                onChange={ handleChange }
                 autoComplete="current-password"
               />
             </Grid>
             <Grid item xs={12}>
               <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
+                control={<Checkbox name="alarm" color="primary" onChange={ handleChange } />}
+                label="기타 안내 및 광고 이메일 받기"
               />
             </Grid>
           </Grid>
@@ -124,23 +196,25 @@ const Register = () => {
             fullWidth
             variant="contained"
             color="primary"
+            size="large"
+            variant="contained"
+            onClick={handleRegister}
             className={classes.submit}
           >
-            Sign Up
+            가입하기
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? Sign in
+              <Link href="/login" variant="body2">
+                이미 가입을 하셨나요? 로그인 하기
               </Link>
             </Grid>
           </Grid>
-        </form>
+        </div>
       </div>
       <Box mt={5}>
         <Copyright />
       </Box>
     </Container>
-  );
+    );
 }
-export default Register;

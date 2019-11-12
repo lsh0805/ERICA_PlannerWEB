c4b7a86b1d89c702b9bca565c8c80e2d35e2a441
lolmc <illionaire1004@gmail.com>
2019-11-13 05:42:45 +0900

    Session 미들웨어 연동

--- server/server.js
+++ server/server.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const fs = require('fs');
 const app = express();
-const port = 5000;
+const api = require('./routes');
+const sequelize = require('./models').sequelize;
 
+sequelize.sync();
+
+const port = 5000;
+app.use('/api', api);
+
+// /* support client-side routing */
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './../client/public/index.html'));
+// });
+
+const sessionData = fs.readFileSync('./config/session.json');
+const sessionSecret = JSON.parse(sessionData);
+
+app.use(session({
+    secret: sessionSecret.secret,
+    resave: false,
+    saveUninitialized: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
+    console.log(req.session);
     res.send("Hello, World!");
 });
 

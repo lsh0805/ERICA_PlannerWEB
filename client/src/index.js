57a1270d3d9f0f79276b7b2670969a8b0287fe48
lolmc <illionaire1004@gmail.com>
2019-12-04 00:13:14 +0900

    Root 컴포넌트 기능 index.js로 이동 및 index로 redux store 이동

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,18 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from 'components/Root';
 import * as serviceWorker from './serviceWorker';
+import {App} from 'pages';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+// Redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from 'reducers';
+import thunk  from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension'
+
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>, document.getElementById('root'));
 serviceWorker.unregister();

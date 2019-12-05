d641dcee0c362c357ca6966c723c98c5fe91e979
lolmc <illionaire1004@gmail.com>
2019-12-06 02:20:31 +0900

    Chart 라이브러리 rechart -> react-chartjs-2로 변경

--- client/src/pages/Info.js
+++ client/src/pages/Info.js
@@ -1,44 +1,35 @@
 import React, {useState} from 'react';
 import DateRangePicker from '@wojtekmaj/react-daterange-picker';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
 import { Chart } from 'components';
-import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-
-const useStyles = makeStyles(theme => ({
-  paper: {
-    padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
-    backgroundColor: '#A5F2F3',
-  },
-  fixedHeight: {
-    height: 240,
-  },
-}));
 
 const Info = (props) => {
-  const classes = useStyles();
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
   const [date, setDate] = useState([new Date(), new Date()]);
   const onDateChange = (date) => {
     setDate(date);
   }
   return (
     <React.Fragment>
-      <div>
-        <DateRangePicker
-          onChange={onDateChange}
-          value={date}
-        />
+      <div className="infoContainer">
+        <div className="infoContents">
+          <div className="userInfo">
+            <div className="userLevel">
+              Lv.10
+            </div>
+            <div className="userLevel">
+              닉네임
+            </div>
+          </div>
+          <div>
+            <DateRangePicker
+              onChange={onDateChange}
+              value={date}
+            />
+          </div>
+          <div className="chartContainer">
+            <Chart/>
+          </div>
+        </div>
       </div>
-      <Grid item xs={12} md={12} lg={8}>
-        <Paper className={fixedHeightPaper}>
-          <Chart/>
-        </Paper>
-      </Grid>
     </React.Fragment>
   );
 };
